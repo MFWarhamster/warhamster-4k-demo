4b62d94ec288c5837c7a8d3ceb18ed1d60a48047
MFWarhamster <ordomailus@warhamster.online>
2025-03-28 22:00:12 -0500

    Fix syntax error in BattleGame

--- src/components/DappUI/Battle/BattleGame.js
+++ src/components/DappUI/Battle/BattleGame.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { buildInitialUnits } from "@/lib/unitBuilder"; // Make sure this exists
+import { buildInitialUnits } from "../../lib/unitBuilder"; // Make sure this exists
 
 export default function BattleGame({ mode }) {
   const [stacks, setStacks] = useState(buildInitialUnits());
